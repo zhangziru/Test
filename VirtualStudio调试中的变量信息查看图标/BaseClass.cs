e8653c633ffd9f546adb6b47ea417467c41aa09f
zhangziru <2401430936@qq.com>
2019-10-30 18:28:18 +0800

    VirtualStudio调试中的变量信息查看图标

---
+++ VirtualStudio调试中的变量信息查看图标/BaseClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp测试项目20181203
+{
+    public class BaseClass
+    {
+        private int Base_Field0 = 0;
+        private int Base_Proppery0_Private { get; set; }
+        protected int Base_Proppery1_Protected { get; set; }
+        public int Base_Proppery2_Public { get; set; }
+      
+    }
+}
---
+++ VirtualStudio调试中的变量信息查看图标/SonClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp测试项目20181203
+{
+    public class SonClass : BaseClass
+    {
+        private int Son_Field0 = 0;
+
+        private int Son_Property0_Private { get; set; }
+        protected int Son_Property1_Protected { get; set; }
+        public int Son_Property2_Public { get; set; }
+    }
+}
