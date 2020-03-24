c5b89506f48e33decb4fe84b0ecec09bc8017042
zhangziru <2401430936@qq.com>
2020-03-24 22:52:28 +0800

    C#-HTTP协议发起请求和上传文件操作

---
+++ C#-通过HTTP协议发起请求和上传文件/HttpPost.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace GoldenWingMau.WMS.Utility
+{
+    public class HttpPost
+    {
+        public CookieContainer cookieContainer { get; set; }
+
+        public string Post(string urlString, Encoding encoding, string postDataString, string ContentType = "application/json")
+        {
+            if (cookieContainer == null)
+            {
+                cookieContainer = new CookieContainer();
+            }
+
+            HttpWebRequest httpWebRequest = null;
+            HttpWebResponse httpWebResponse = null;
+            Stream inputStream = null;
+            Stream outputStream = null;
+            StreamReader streamReader = null;
+            string htmlString = string.Empty;
+
+            byte[] postDataByte = encoding.GetBytes(postDataString); //转换POST数据  
+
+            //建立页面请求 
+            ServicePointManager.ServerCertificateValidationCallback =
+new RemoteCertificateValidationCallback(CheckValidationResult);//SSl验证
+            try
+            {
+                httpWebRequest = WebRequest.Create(urlString) as HttpWebRequest;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("建立页面请求时发生错误！", ex);
+            }
+
+            //指定请求处理方式  
+
+            httpWebRequest.Method = "POST";
+            httpWebRequest.KeepAlive = false;
+            httpWebRequest.ContentType = ContentType;
+            httpWebRequest.CookieContainer = cookieContainer;
+            httpWebRequest.ContentLength = postDataByte.Length;
+
+            //向服务器传送数据  
+            try
+            {
+                inputStream = httpWebRequest.GetRequestStream();
+                inputStream.Write(postDataByte, 0, postDataByte.Length);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("发送POST数据时发生错误！", ex);
+            }
+            finally
+            {
+                inputStream.Close();
+            }
+
+            //接受服务器返回信息  
+
+            try
+            {
+
+                httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                outputStream = httpWebResponse.GetResponseStream();
+                streamReader = new StreamReader(outputStream, encoding);
+                htmlString = streamReader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("接受服务器返回页面时发生错误！", ex);
+            }
+            finally
+            {
+                streamReader.Close();
+            }
+
+            //获取cookies
+            foreach (Cookie cookie in httpWebResponse.Cookies)
+            {
+                cookieContainer.Add(cookie);
+            }
+
+            return htmlString;
+
+        }
+
+        public bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {   // 总是接受  
+            return true;
+        }
+    }
+
+    internal class AcceptAllCertificatePolicy : ICertificatePolicy
+    {
+        public AcceptAllCertificatePolicy()
+        {
+        }
+
+        public bool CheckValidationResult(ServicePoint sPoint, X509Certificate cert, WebRequest wRequest, int certProb)
+        {
+            return true;
+        }
+    }
+}
---
+++ C#-通过HTTP协议发起请求和上传文件/HttpPostFile.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace GoldenWingMau.WMS.Utility
+{
+    public class HttpPostFile
+    {
+        public void Post(string urlString,string FilePath,string saveName)
+        {
+            FileStream fs = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader r = new BinaryReader(fs);
+
+       
+
+            //时间戳
+            string strBoundary = "----------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundaryBytes = Encoding.ASCII.GetBytes("\r\n--" + strBoundary + "\r\n");
+
+            //请求头部信息
+            StringBuilder sb = new StringBuilder();
+            sb.Append("--");
+            sb.Append(strBoundary);
+            sb.Append("\r\n");
+            sb.Append("Content-Disposition: form-data; name=\"");
+            sb.Append("file");
+            sb.Append("\"; filename=\"");
+            sb.Append(saveName);
+            sb.Append("\"");
+            sb.Append("\r\n");
+            sb.Append("Content-Type: ");
+            sb.Append("application/octet-stream");
+            sb.Append("\r\n");
+            sb.Append("\r\n");
+
+            string strPostHeader = sb.ToString();
+            byte[] postHeaderBytes = Encoding.UTF8.GetBytes(strPostHeader);
+
+            // 根据uri创建HttpWebRequest对象
+            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(new Uri(urlString));
+            httpReq.Method = "POST";
+
+            //对发送的数据不使用缓存【重要、关键】
+            httpReq.AllowWriteStreamBuffering = false;
+
+            //设置获得响应的超时时间（300秒）
+            httpReq.Timeout = 300000;
+            httpReq.ContentType = "multipart/form-data; boundary=" + strBoundary;
+            long length = fs.Length + postHeaderBytes.Length + boundaryBytes.Length;
+            long fileLength = fs.Length;
+            httpReq.ContentLength = length;
+            try
+            {
+
+                //每次上传4k
+                int bufferLength = 4096;
+                byte[] buffer = new byte[bufferLength];
+
+                //已上传的字节数
+                long offset = 0;
+
+                //开始上传时间
+                DateTime startTime = DateTime.Now;
+                int size = r.Read(buffer, 0, bufferLength);
+                Stream postStream = httpReq.GetRequestStream();
+
+                //发送请求头部消息
+                postStream.Write(postHeaderBytes, 0, postHeaderBytes.Length);
+                while (size > 0)
+                {
+                    postStream.Write(buffer, 0, size);
+                    offset += size;
+                    size = r.Read(buffer, 0, bufferLength);
+                }
+                //添加尾部的时间戳
+                postStream.Write(boundaryBytes, 0, boundaryBytes.Length);
+                postStream.Close();
+
+                //获取服务器端的响应
+                WebResponse webRespon = httpReq.GetResponse();
+                Stream s = webRespon.GetResponseStream();
+                StreamReader sr = new StreamReader(s);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("发送POST数据时发生错误！", ex);
+            }
+            finally
+            {
+                fs.Close();
+                r.Close();
+            }
+        }
+		
+		//服务器端处理的Demo
+		
+		//public void ServerHandle()
+        //{
+        //    HttpFileCollection files = Request.Files;
+        //    string filePath = Server.MapPath("~/UploadFiles/");
+        //    if (files.Count != 0)
+        //    {
+        //        string fileName = files[0].FileName;
+        //        files[0].SaveAs(Path.Combine(filePath, fileName));
+        //        Response.Write("<p>上传成功</p>");
+        //    }
+        //    else
+        //    {
+        //        Response.Write("<p>未获取到Files:" + files.Count.ToString() + "</p>");
+        //    }
+        //}		
+    }
+}
